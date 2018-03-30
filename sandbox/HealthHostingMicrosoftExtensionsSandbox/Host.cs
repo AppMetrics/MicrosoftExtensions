a29c676205420c3539fa3bd81ac304badc3ccaf4
alhardy <alhardy@users.noreply.github.com>
2018-03-30 23:29:50 +1100

    AppMetrics/AppMetrics#243 #5 Adding hosting extensions to simplify bootstrapping appmetrics health using net cores HostBuilder

---
+++ sandbox/HealthHostingMicrosoftExtensionsSandbox/Host.cs
@@ -0,0 +1,78 @@
+﻿// <copyright file="Host.cs" company="Allan Hardy">
+// Copyright (c) Allan Hardy. All rights reserved.
+// </copyright>
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using App.Metrics.Health;
+using App.Metrics.Health.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using static System.Console;
+
+namespace HealthHostingMicrosoftExtensionsSandbox
+{
+    public class Host
+    {
+        public static async Task Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                         .MinimumLevel.Verbose()
+                         .WriteTo.LiterateConsole(LogEventLevel.Information)
+                         .WriteTo.Seq("http://localhost:5341", LogEventLevel.Verbose)
+                         .CreateLogger();
+
+            var host = new HostBuilder().ConfigureAppConfiguration(
+                (hostContext, config) =>
+                {
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+                    config.AddEnvironmentVariables();
+                    config.AddJsonFile("appsettings.json", optional: true);
+                    config.AddCommandLine(args);
+                }).ConfigureHealthWithDefaults(
+                (context, builder) =>
+                {
+                    builder.OutputHealth.AsPlainText()
+                           .OutputHealth.AsJson()
+                           .HealthChecks.AddCheck("inline-check", () => new ValueTask<HealthCheckResult>(HealthCheckResult.Healthy()));
+                }).Build();
+
+            var health = host.Services.GetRequiredService<IHealthRoot>();
+
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            await host.RunUntilEscAsync(
+                TimeSpan.FromSeconds(5),
+                cancellationTokenSource,
+                async () =>
+                {
+                    Clear();
+
+                    var healthStatus = await health.HealthCheckRunner.ReadAsync(cancellationTokenSource.Token);
+
+                    foreach (var formatter in health.OutputHealthFormatters)
+                    {
+                        WriteLine($"Formatter: {formatter.GetType().FullName}");
+                        WriteLine("-------------------------------------------");
+
+                        using (var stream = new MemoryStream())
+                        {
+                            await formatter.WriteAsync(stream, healthStatus, cancellationTokenSource.Token);
+
+                            var result = Encoding.UTF8.GetString(stream.ToArray());
+
+                            WriteLine(result);
+                        }
+                    }
+                });
+
+            await host.RunAsync(token: cancellationTokenSource.Token);
+        }
+    }
+}
---
+++ src/App.Metrics.Health.Extensions.Hosting/HealthHostBuilderExtensions.cs
@@ -0,0 +1,123 @@
+﻿// <copyright file="HealthHostBuilderExtensions.cs" company="Allan Hardy">
+// Copyright (c) Allan Hardy. All rights reserved.
+// </copyright>
+
+using System;
+using System.Reflection;
+using App.Metrics.Health.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyModel;
+using Microsoft.Extensions.Hosting;
+
+namespace App.Metrics.Health.Extensions.Hosting
+{
+    public static class HealthHostBuilderExtensions
+    {
+        private static bool _healthBuilt;
+
+        public static IHostBuilder ConfigureHealthWithDefaults(
+            this IHostBuilder hostBuilder,
+            Action<HostBuilderContext, IHealthBuilder> configureHealth,
+            DependencyContext dependencyContext = null)
+        {
+            if (_healthBuilt)
+            {
+                throw new InvalidOperationException("HealthBuilder allows creation only of a single instance of IMetrics");
+            }
+
+            return hostBuilder.ConfigureServices(
+                (context, services) =>
+                {
+                    var healthBuilder = AppMetricsHealth.CreateDefaultBuilder();
+                    configureHealth(context, healthBuilder);
+                    healthBuilder.HealthChecks.RegisterFromAssembly(services, dependencyContext ?? GetDependencyContext());
+                    healthBuilder.Configuration.ReadFrom(context.Configuration);
+                    healthBuilder.BuildAndAddTo(services);
+                    _healthBuilt = true;
+                });
+        }
+
+        public static IHostBuilder ConfigureHealthWithDefaults(this IHostBuilder hostBuilder, Action<IHealthBuilder> configureHealth)
+        {
+            if (_healthBuilt)
+            {
+                throw new InvalidOperationException("HealthBuilder allows creation only of a single instance of IHealth");
+            }
+
+            hostBuilder.ConfigureHealthWithDefaults(
+                (context, builder) =>
+                {
+                    configureHealth(builder);
+                });
+
+            return hostBuilder;
+        }
+
+        public static IHostBuilder ConfigureHealth(
+            this IHostBuilder hostBuilder,
+            Action<HostBuilderContext, IHealthBuilder> configureHealth)
+        {
+            if (_healthBuilt)
+            {
+                throw new InvalidOperationException("HealthBuilder allows creation only of a single instance of IHealth");
+            }
+
+            return hostBuilder.ConfigureServices(
+                (context, services) =>
+                {
+                    services.AddHealth(
+                        healthBuilder =>
+                        {
+                            configureHealth(context, healthBuilder);
+                            healthBuilder.Configuration.ReadFrom(context.Configuration);
+                            _healthBuilt = true;
+                        });
+                });
+        }
+
+        public static IHostBuilder ConfigureHealth(this IHostBuilder hostBuilder, Action<IHealthBuilder> configureHealth)
+        {
+            if (_healthBuilt)
+            {
+                throw new InvalidOperationException("HealthBuilder allows creation only of a single instance of IHealth");
+            }
+
+            hostBuilder.ConfigureHealth(
+                (context, healthBuilder) =>
+                {
+                    configureHealth(healthBuilder);
+                });
+
+            return hostBuilder;
+        }
+
+        public static IHostBuilder ConfigureHealth(
+            this IHostBuilder hostBuilder,
+            DependencyContext dependencyContext = null)
+        {
+            if (_healthBuilt)
+            {
+                return hostBuilder;
+            }
+
+            return hostBuilder.ConfigureServices(
+                (context, services) =>
+                {
+                    if (!_healthBuilt)
+                    {
+                        AppMetricsHealth.CreateDefaultBuilder()
+                            .Configuration.ReadFrom(context.Configuration)
+                            .HealthChecks.RegisterFromAssembly(services, dependencyContext ?? GetDependencyContext())
+                            .BuildAndAddTo(services);
+
+                        _healthBuilt = true;
+                    }
+                });
+        }
+
+        internal static DependencyContext GetDependencyContext()
+        {
+            return Assembly.GetEntryAssembly() != null ? DependencyContext.Default : null;
+        }
+    }
+}
