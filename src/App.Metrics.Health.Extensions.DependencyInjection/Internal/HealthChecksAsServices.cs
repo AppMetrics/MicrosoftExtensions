0134d5f5164a6a8b9fedf51290bef60e8987e6fd
alhardy <alhardy@users.noreply.github.com>
2018-05-11 22:33:12 +1000

    #6 net core 2.1.0-rc1, updating app metrics ref to 3.0.0, updating copy right headers

--- src/App.Metrics.Health.Extensions.DependencyInjection/Internal/HealthChecksAsServices.cs
+++ src/App.Metrics.Health.Extensions.DependencyInjection/Internal/HealthChecksAsServices.cs
@@ -1,5 +1,5 @@
-﻿// <copyright file="HealthChecksAsServices.cs" company="Allan Hardy">
-// Copyright (c) Allan Hardy. All rights reserved.
+﻿// <copyright file="HealthChecksAsServices.cs" company="App Metrics Contributors">
+// Copyright (c) App Metrics Contributors. All rights reserved.
 // </copyright>
 
 using System;
